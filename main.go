74ed3bfb1773c88ed23cd77be741c9759b003905
n.kuzmin <n.kuzmin@fbs.com>
2022-10-11 22:56:18 +0600

    speed up (new algorithm, -145 sec)

--- main.go
+++ main.go
@@ -7,56 +7,62 @@ import (
 	"image/jpeg"
 	"math"
 	"os"
+	"time"
 )
 
 var names = []string{"photo_5341437942142451795_y.jpg", "photo_5341437942142451796_y.jpg"}
 
 func main() {
+	var start = time.Now()
 	var images = readAll(names)
 
-	var a = imageToRows(images[0], "0.png")
-	var b = imageToRows(images[1], "1.png")
+	y1, y2, length := overlaps(images[0], images[1])
+	fmt.Println(y1, y2, length)
+	fmt.Println(time.Since(start).String())
 
-	var originY, otherY, length = a.overlaps(b)
-
-	fmt.Println(originY, otherY, length)
-
-	// debug print union
-	var res = image.NewRGBA(image.Rect(0, 0, 1440, length))
-	for x := 0; x < 720; x++ {
-		for y := originY; y < originY+length; y++ {
-			res.Set(x, y-originY, a.original.At(x, y))
-		}
-	}
-
-	for x := 0; x < 720; x++ {
-		for y := otherY; y < otherY+length; y++ {
-			res.Set(x+720, y-otherY, b.original.At(x, y))
-		}
-	}
-
-	f, _ := os.Create("union.jpg")
-	jpeg.Encode(f, res, nil)
-	f.Close()
-
-	// print final image
-	var sizeOfOrigin = originY + length
-	var sizeOfOther = images[1].Bounds().Dy() - length - otherY
-	var res2 = image.NewRGBA(image.Rect(0, 0, images[0].Bounds().Dx(), sizeOfOrigin+sizeOfOther))
-
-	for x := 0; x < images[0].Bounds().Dx(); x++ {
-		for y := 0; y < originY+length; y++ {
-			res2.Set(x, y, images[0].At(x, y))
-		}
-
-		for y := otherY + length; y < images[1].Bounds().Dy(); y++ {
-			res2.Set(x, y-otherY-length+sizeOfOrigin, images[1].At(x, y))
-		}
-	}
-
-	f1, _ := os.Create("woohoo.jpg")
-	jpeg.Encode(f1, res2, nil)
-	f1.Close()
+	// var a = imageToRows(images[0], "0.png")
+	// var b = imageToRows(images[1], "1.png")
+	//
+	// var originY, otherY, length = a.overlaps(b)
+	//
+	// fmt.Println(originY, otherY, length)
+	//
+	// // debug print union
+	// var res = image.NewRGBA(image.Rect(0, 0, 1440, length))
+	// for x := 0; x < 720; x++ {
+	// 	for y := originY; y < originY+length; y++ {
+	// 		res.Set(x, y-originY, a.original.At(x, y))
+	// 	}
+	// }
+	//
+	// for x := 0; x < 720; x++ {
+	// 	for y := otherY; y < otherY+length; y++ {
+	// 		res.Set(x+720, y-otherY, b.original.At(x, y))
+	// 	}
+	// }
+	//
+	// f, _ := os.Create("union.jpg")
+	// jpeg.Encode(f, res, nil)
+	// f.Close()
+	//
+	// // print final image
+	// var sizeOfOrigin = originY + length
+	// var sizeOfOther = images[1].Bounds().Dy() - length - otherY
+	// var res2 = image.NewRGBA(image.Rect(0, 0, images[0].Bounds().Dx(), sizeOfOrigin+sizeOfOther))
+	//
+	// for x := 0; x < images[0].Bounds().Dx(); x++ {
+	// 	for y := 0; y < originY+length; y++ {
+	// 		res2.Set(x, y, images[0].At(x, y))
+	// 	}
+	//
+	// 	for y := otherY + length; y < images[1].Bounds().Dy(); y++ {
+	// 		res2.Set(x, y-otherY-length+sizeOfOrigin, images[1].At(x, y))
+	// 	}
+	// }
+	//
+	// f1, _ := os.Create("woohoo.jpg")
+	// jpeg.Encode(f1, res2, nil)
+	// f1.Close()
 }
 
 func readAll(names []string) []image.Image {
@@ -77,20 +83,6 @@ func readAll(names []string) []image.Image {
 	}
 
 	return res
-}
-
-type ImageRows struct {
-	original image.Image
-	rows     []string
-}
-
-func imageToRows(img image.Image, name string) *ImageRows {
-	var rows = make([]string, img.Bounds().Dy())
-
-	return &ImageRows{
-		original: img,
-		rows:     rows,
-	}
 }
 
 // mask is never empty
@@ -122,67 +114,6 @@ func getLongestSequenceOfTrue(mask []bool) (pos, length int) {
 	return pos, length
 }
 
-func (origin *ImageRows) overlaps(other *ImageRows) (originY, otherY, length int) {
-	var (
-		size        = origin.original.Bounds().Dy()
-		originSlice = getTwoDimensionSlice(origin.original, color.White, size)
-	)
-
-	for i := 0; i < size*2; i++ {
-		var otherSlice = getTwoDimensionSlice(other.original, color.Black, i)
-
-		var mask = getRowBoolMask(originSlice, otherSlice)
-
-		// now get the longest 'true' sequence
-		var maxPos, maxLen = getLongestSequenceOfTrue(mask)
-
-		fmt.Println("i:", i)
-		if maxLen > length {
-			length = maxLen
-			originY = maxPos - size
-			otherY = maxPos - i
-
-			fmt.Println("Var:", originY, otherY, length)
-		}
-	}
-
-	return originY, otherY, length
-}
-
-func getTwoDimensionSlice(img image.Image, base color.Color, offset int) [][]color.Color {
-	var res = make([][]color.Color, img.Bounds().Dy()*3)
-
-	for i := 0; i < len(res); i++ {
-		res[i] = make([]color.Color, img.Bounds().Dx())
-		for j := range res[i] {
-			if i < offset || i >= offset+img.Bounds().Dy() {
-				res[i][j] = base
-			} else {
-				res[i][j] = img.At(j, i-offset)
-			}
-		}
-	}
-
-	return res
-}
-
-func getRowBoolMask(sl1, sl2 [][]color.Color) []bool {
-	var res = make([]bool, len(sl1))
-
-	for i := range sl1 {
-		var allSame = true
-		for j := range sl1[i] {
-			if !closeEnough(sl1[i][j], sl2[i][j]) {
-				allSame = false
-				break
-			}
-		}
-		res[i] = allSame
-	}
-
-	return res
-}
-
 func closeEnough(c1, c2 color.Color) bool {
 	var (
 		r1, g1, b1, _ = c1.RGBA()
@@ -194,4 +125,65 @@ func closeEnough(c1, c2 color.Color) bool {
 	)
 	res := math.Sqrt(diffR*diffR + diffG*diffG + diffB*diffB)
 	return res < 60000
+}
+
+func overlaps(i1, i2 image.Image) (y1, y2, length int) {
+	for offset := 1; offset < i1.Bounds().Dy()*2; offset++ {
+		var (
+			from1 = offset - i1.Bounds().Dy()
+			to1   = from1 + i1.Bounds().Dy()
+			from2 = i2.Bounds().Dy() - offset
+			to2   = from2 + i2.Bounds().Dy()
+		)
+		if from1 < 0 {
+			from1 = 0
+		}
+		if to1 > i1.Bounds().Dy() {
+			to1 = i1.Bounds().Dy()
+		}
+		if from2 < 0 {
+			from2 = 0
+		}
+		if to2 > i2.Bounds().Dy() {
+			to2 = i2.Bounds().Dy()
+		}
+
+		if to1-from1 != to2-from2 {
+			fmt.Println(to1-from1, to2-from2)
+			panic("damn")
+		}
+
+		var hasTrues = false
+		var mask = make([]bool, to1-from1)
+
+		for yOffset := 0; yOffset < to1-from1; yOffset++ {
+			var allSame = true
+			for x := 0; x < i1.Bounds().Dx(); x++ {
+				if !closeEnough(i1.At(x, from1+yOffset), i2.At(x, from2+yOffset)) {
+					allSame = false
+					break
+				}
+			}
+			if allSame == true {
+				hasTrues = true
+			}
+			mask[yOffset] = allSame
+		}
+
+		var maxPos, maxLen = 0, 0
+		if hasTrues {
+			maxPos, maxLen = getLongestSequenceOfTrue(mask)
+		}
+
+		// fmt.Println("i:", offset)
+		if maxLen > length {
+			length = maxLen
+			y1 = maxPos + from1
+			y2 = maxPos + from2
+
+			// fmt.Println("Var:", y1, y2, length)
+		}
+	}
+
+	return y1, y2, length
 }
